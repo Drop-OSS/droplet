86bb269d81ffe58b87ba59dad68c87a06260d867
DecDuck <declanahofmeyr@gmail.com>
2024-10-07 12:13:24 +1100

    cleanup

--- test.mjs
+++
@@ -1,6 +0,0 @@
-import { repack } from './index.js';
-
-const source = "/home/decduck/.steam/steam/steamapps/common/ClusterTruck";
-const output = "/home/decduck/Dev/droplet-output";
-
-await repack(source, output);
